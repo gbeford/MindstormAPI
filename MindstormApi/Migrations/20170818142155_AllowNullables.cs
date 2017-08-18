b696906041a76edcf7a5a8178cac164ed3822179
gbeford <gbeford@gmail.com>
2017-08-18 10:40:06 -0400

    try to update nullable fields

---
+++ MindstormApi/Migrations/20170818142155_AllowNullables.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace MindstormApi.Migrations
+{
+    public partial class AllowNullables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "MinRange",
+                table: "MatchDefinition",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AlterColumn<int>(
+                name: "MaxRange",
+                table: "MatchDefinition",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AlterColumn<int>(
+                name: "MatchYear",
+                table: "MatchDefinition",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(DateTime));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "MinRange",
+                table: "MatchDefinition",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "MaxRange",
+                table: "MatchDefinition",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "MatchYear",
+                table: "MatchDefinition",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "DateTime");
+        }
+    }
+}
