163f7663a7f6714793b0f0d0456682364224ab12
gbeford <gbeford@gmail.com>
2017-08-29 17:26:32 -0400

    add range list

--- MindstormApi/Models/MissionItem.cs
+++ MindstormApi/Models/MissionItem.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 namespace MindstormApi.Models
 {
     public class MissionItem
-    {       
+    {
         public int MissionItemId { get; set; }
         public string QuestionDescription { get; set; }
         public string Type { get; set; } // bool / range/ string list
@@ -16,6 +16,32 @@ namespace MindstormApi.Models
         public int SortOrder { get; set; }
         public int MissionId { get; set; }
         public Mission Mission { get; set; }
-     
+        public string[] OptionListArray
+        {
+            get
+            {
+                return string.IsNullOrEmpty(OptionList) ? null : OptionList.Split(',');
+            }
+        }
+        public List<string> RangeListArray
+        {
+            get
+            {
+                if (Type == "range")
+                {
+                    var RangeList = new List<string>();
+                    for (var x = MinRange; x <= MaxRange; x++)
+                    {
+                        RangeList.Add(x.ToString());
+                    }
+                    return RangeList;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+
+        }
     }
 }
